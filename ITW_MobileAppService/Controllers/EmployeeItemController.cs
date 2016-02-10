9341a1709da52dea7a368e4bdb0d6814302d337a
Osameshar <cakeller42@gmail.com>
2016-02-10 16:52:44 -0600

    AddedDatabaseTypesControllers

---
+++ ITW_MobileAppService/Controllers/EmployeeItemController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server;
+using ITW_MobileAppService.DataObjects;
+using ITW_MobileAppService.Models;
+
+namespace ITW_MobileAppService.Controllers
+{
+    public class EmployeeItemController : TableController<EmployeeItem>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            ITW_MobileAppContext context = new ITW_MobileAppContext();
+            DomainManager = new EntityDomainManager<EmployeeItem>(context, Request);
+        }
+
+        // GET tables/TodoItem
+        public IQueryable<EmployeeItem> GetAllEmployeeItems()
+        {
+            return Query();
+        }
+
+        // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<EmployeeItem> GetEmployeeItem(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<EmployeeItem> PatchEmployeeItem(string id, Delta<EmployeeItem> patch)
+        {
+            return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TodoItem
+        public async Task<IHttpActionResult> PostEmployeeItem(EmployeeItem item)
+        {
+            EmployeeItem current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteEmployeeItem(string id)
+        {
+            return DeleteAsync(id);
+        }
+    }
+}
---
+++ ITW_MobileAppService/DataObjects/DatabaseItemTypes.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System;
+
+namespace ITW_MobileAppService.DataObjects
+{
+    public class EventItem : EntityData
+    {
+        public string Name { get; set; }
+
+        public string EventRecipients { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public DateTime EventTime { get; set; }
+
+        public string Location { get; set; }
+
+        public string Category { get; set; }
+
+        public int EventPriority { get; set; }
+
+        public string EventDescription { get; set; }
+
+        public int EventID { get; set; }
+
+        public int EmployeeID { get; set; }
+
+        public bool deleted { get; set; }
+    }
+    public class EmployeeItem : EntityData
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int EmployeeID { get; set; }
+
+        public string Department { get; set; }
+
+        public string PrivledgeLevel { get; set; }
+    }
+    public class RecipientListItem : EntityData
+    {
+        public int EmployeeID { get; set; }
+
+        public int EventID { get; set; }
+    }
+}
