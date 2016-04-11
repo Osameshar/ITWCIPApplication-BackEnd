b5fb1cc3dc12dd9c80b0434d214bd3c80e2586b3
Osameshar <cakeller42@gmail.com>
2016-04-11 16:28:28 -0500

    Revert "TestPush"
    This reverts commit 2117d34a26cd9bf44fef34a92578bb4e325246bd.

--- ITW_MobileAppService/Controllers/EventItemController.cs
+++ ITW_MobileAppService/Controllers/EventItemController.cs
@@ -6,20 +6,11 @@ using System.Web.Http.OData;
 using Microsoft.Azure.Mobile.Server;
 using ITW_MobileAppService.DataObjects;
 using ITW_MobileAppService.Models;
-using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using Newtonsoft.Json.Linq;
 
 namespace ITW_MobileAppService.Controllers
 {
     public class EventItemController : TableController<EventItem>
     {
-        public const string API_KEY = "AIzaSyD0FEqhq4UA9I97yEgGjla2wBIRDuKsq5I";
-        public const string MESSAGE = "Push Message";
-
-
         protected override void Initialize(HttpControllerContext controllerContext)
         {
             base.Initialize(controllerContext);
@@ -49,7 +40,6 @@ namespace ITW_MobileAppService.Controllers
         public async Task<IHttpActionResult> PostEventItem(EventItem item)
         {
             EventItem current = await InsertAsync(item);
-            sendPush("global");
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
         }
 
@@ -57,42 +47,6 @@ namespace ITW_MobileAppService.Controllers
         public Task DeleteEventItem(string id)
         {
             return DeleteAsync(id);
-        }
-
-        private void sendPush(string topic)
-        {
-            var jGcmData = new JObject();
-            var jData = new JObject();
-
-            jData.Add("message", MESSAGE);
-            jGcmData.Add("to", "/topics/" + topic);
-            jGcmData.Add("data", jData);
-
-            var url = new Uri("https://gcm-http.googleapis.com/gcm/send");
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    client.DefaultRequestHeaders.Accept.Add(
-                        new MediaTypeWithQualityHeaderValue("application/json"));
-
-                    client.DefaultRequestHeaders.TryAddWithoutValidation(
-                        "Authorization", "key=" + API_KEY);
-
-                    Task.WaitAll(client.PostAsync(url,
-                        new StringContent(jGcmData.ToString(), Encoding.Default, "application/json"))
-                            .ContinueWith(response =>
-                            {
-                                Console.WriteLine(response);
-                                Console.WriteLine("Message sent: check the client device notification tray.");
-                            }));
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unable to send GCM message:");
-                Console.Error.WriteLine(e.StackTrace);
-            }
         }
     }
 }
